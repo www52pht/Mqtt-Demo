d7051b37f2caf7d76163fe37d006c2a7dd7da158
wx-wangww3 <1780910546@qq.com>
2025-01-04 14:20:38 +0800

    test

--- src/main/java/com/example/mqttdemo/MqttDemoApplication.java
+++ src/main/java/com/example/mqttdemo/MqttDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MqttDemoApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MqttDemoApplication.class, args);
     }
 
